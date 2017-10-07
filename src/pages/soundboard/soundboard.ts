97fc20973ae5864ab2738463e5a7f1e76f034b8e
Dimitri Dessus <iphoneretro@icloud.com>
2017-10-07 14:16:33 +0200

    [Add] Media plugin implementation

--- src/pages/soundboard/soundboard.ts
+++ src/pages/soundboard/soundboard.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// Mapping data
+import * as SoundboardMapping from '../../models/soundboard.mapping';
+
+// Native
+import { Media, MediaObject } from '@ionic-native/media';
+
 @Component({
   selector: 'page-soundboard',
   templateUrl: 'soundboard.html'
 })
 export class SoundboardPage {
 
-  constructor(public navCtrl: NavController) {
+  private soundboardData: SoundboardMapping.SoundboardMap[];
+  private file: MediaObject;
 
+  constructor(public navCtrl: NavController, private media: Media) {
+    this.soundboardData = SoundboardMapping.SoundboardMock;
+  }
+
+  public playSound(ressource: string) {
+    if (this.file) {
+      this.file.stop();
+      this.file.release();
+    }
+
+    this.file = this.media.create('./assets/' + ressource);
+    this.file.play();
   }
 
 }
